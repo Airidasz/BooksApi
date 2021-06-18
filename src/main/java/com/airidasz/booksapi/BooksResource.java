d216e71ca5491d3a833a1055cb0fde0c09a273f2
Airidasz <61290246+Airidasz@users.noreply.github.com>
2021-06-18 15:38:45 +0300

    Added comments and fixed minor bugs

--- src/main/java/com/airidasz/booksapi/BooksResource.java
+++ src/main/java/com/airidasz/booksapi/BooksResource.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,10 +18,12 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@Path("books")
+@Path("/books")
 public class BooksResource {
 	private BookDAO books = BookDAO.getInstance();
 
+	// Lists books grouped by quantity
+	// Data is sorted by total price
     @GET
     @Produces("application/json")
     public Response getBooks() {    	
@@ -33,17 +36,63 @@ public class BooksResource {
     	for(Book book : sortedBooks) {
     		booksByQuantity.computeIfAbsent(book.getQuantity(), k -> new ArrayList<>()).add(book);
     	}
-    	
+
     	Status status = Response.Status.OK;
     	if(sortedBooks.size() <= 0)
-    		status = Response.Status.NO_CONTENT;
+    		status = Response.Status.NOT_FOUND;
     	
-        return Response
+    	return Response
 	    	      .status(status)
 	    	      .entity(booksByQuantity)
 	    	      .build();
     }
     
+    // Adds books to DAO instance
+ 	// Books are of three types
+    // the type of book is selected by looking for certain fields
+    // in request
+    @POST
+    @Produces("application/json")
+    public Response addBook(String request){
+    	Book book = null;
+    	try {
+    		ObjectMapper objectMapper = new ObjectMapper();
+    		JsonNode jsonNode = objectMapper.readTree(request);
+			
+    		// Mapping class chosen by field
+    		if (jsonNode.get("scienceIndex") != null)
+    			book = objectMapper.readValue(request, ScientificJournal.class);
+    		else if(jsonNode.get("releaseYear") != null)
+    			book = objectMapper.readValue(request, AntiqueBook.class);	
+			else
+				book = objectMapper.readValue(request, Book.class);	
+    		
+    		// Null value check
+    		if(book.hasNullValues())
+    			return Response.status(Response.Status.BAD_REQUEST)
+        				.entity("{ \"error\": \"Required fields missing\"}")
+        				.build();
+    		
+    		// Check for book with same barcode
+    		if(books.get(book.getBarcode()) != null)
+        		return Response.status(Response.Status.BAD_REQUEST)
+        				.entity("{ \"error\": \"Book with same barcode exists\"}")
+        				.build();
+				
+	    	books.add(book);
+
+		} catch (JsonMappingException e) {
+			// This triggers when there are extra fields in request or when setting scienceIndex or releaseYear 
+			return Response.status(Response.Status.BAD_REQUEST).entity("{ \"error\": \"Bad format\"}").build();
+		} catch (JsonProcessingException e) {
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("{}").build();
+		}
+    	
+    	return Response.status(Response.Status.CREATED).build();
+    }
+    
+    // Get total price of book
+    // total price calculations depend on book type
     @GET
     @Produces("application/json")
     @Path("/{barcode}/price")
@@ -61,6 +110,7 @@ public class BooksResource {
         	      .build();
     }
     
+    // Find book using its barcode
     @GET
     @Produces("application/json")
     @Path("/{barcode}")
@@ -78,9 +128,10 @@ public class BooksResource {
         	      .build();
     }
     
-    @POST
+    // Update chosen book
+    @PUT
     @Produces("application/json")
-    @Path("/{barcode}/update")
+    @Path("/{barcode}")
     public Response updateBook(@PathParam("barcode") String barcode, String request) {
     	Book book = books.get(barcode);
     	
@@ -91,58 +142,31 @@ public class BooksResource {
     		
     	
     	Book updatedBook = null;
+    	ObjectMapper objectMapper = new ObjectMapper();
     	try {
-    		ObjectMapper objectMapper = new ObjectMapper();
     		updatedBook = objectMapper.readValue(request, book.getClass());	
+    		JsonNode jsonNode = objectMapper.readTree(request);
+    		
+    		// Check for barcode changes
+        	if(jsonNode.get("barcode") != null) {
+        		if(updatedBook.getBarcode() != barcode && books.get(updatedBook.getBarcode()) != null) {
+        			return Response.status(Response.Status.BAD_REQUEST)
+        					.entity("{ \"error\": \"Updated barcode is used by different book\"}")
+        					.build();
+        		}
+        	}
 		} catch (JsonMappingException e) {
 			return Response.status(Response.Status.BAD_REQUEST).entity("{ \"error\": \"Bad format\"}").build();
 		} catch (JsonProcessingException e) {
 			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("{}").build();
 		}
-    	updatedBook.setBarcode(book.getBarcode());
-    	books.update(updatedBook);
     	
-    	book = books.get(book.getBarcode());
+    	books.update(barcode, updatedBook);
+    	
         return Response
         	      .status(Response.Status.OK)
-        	      .entity(book)
         	      .build();
     }
     
-    
-    @POST
-    @Produces("application/json")
-    public Response addBook(String request){
-    	Book book = null;
-    	try {
-    		ObjectMapper objectMapper = new ObjectMapper();
-    		JsonNode jsonNode = objectMapper.readTree(request);
-			
-    		if (jsonNode.get("scienceIndex") != null)
-    			book = objectMapper.readValue(request, ScientificJournal.class);
-    		else if(jsonNode.get("releaseYear") != null)
-    			book = objectMapper.readValue(request, AntiqueBook.class);	
-			else
-				book = objectMapper.readValue(request, Book.class);	
-    		
-    		if(book.hasNullValues())
-    			return Response.status(Response.Status.BAD_REQUEST)
-        				.entity("{ \"error\": \"Required fields missing\"}")
-        				.build();
-    		
-    		if(books.get(book.getBarcode()) != null)
-        		return Response.status(Response.Status.BAD_REQUEST)
-        				.entity("{ \"error\": \"Book with same barcode exists\"}")
-        				.build();
-				
-	    	books.add(book);
-
-		} catch (JsonMappingException e) {
-			return Response.status(Response.Status.BAD_REQUEST).entity("{ \"error\": \"Bad format\"}").build();
-		} catch (JsonProcessingException e) {
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("{}").build();
-		}
-    	
-    	return Response.status(Response.Status.CREATED).entity(book).build();
-    }
+   
 }
